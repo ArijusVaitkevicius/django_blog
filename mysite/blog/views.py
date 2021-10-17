a981a09cf2738733f6c864374de20f5c99405fb7
ArijusVaitkevicius <arijus1990@gmail.com>
2021-10-17 23:05:23 +0300

    4 punktas + article.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import generic
 from .models import Article
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -11,7 +10,12 @@ def index(request):
 class ArticlesListView(generic.ListView):
     model = Article
     template_name = 'articles.html'
-    paginate_by = 1
+    paginate_by = 2
 
     def get_queryset(self):
         return Article.objects.order_by('-date')
+
+
+def article(request, article_id):
+    single_article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'article.html', {'article': single_article})
